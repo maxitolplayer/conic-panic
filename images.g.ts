a0e0f54b7e88be9d6f5442eea0126185582d191e
maxitolplayer <119235578+maxitolplayer@users.noreply.github.com>
2024-03-04 13:09:53 -0800

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,471 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "Idle":
+            case "anim1":return [img`
+f f f f f f f f f f f f f f f f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f f f f f f f f f f f f f f f f 
+`, img`
+f f f f f f f f f f f f f f f f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 f f 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 f f 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 f f 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 f f 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 f f 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 f f f f f f f f 
+f 1 1 1 1 1 1 1 1 1 f f f f f f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f f f f f f f f f f f f f f f f 
+`, img`
+f f f f f f f f f f f f f f f f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 f f 1 1 1 1 1 f f 
+f 1 1 1 1 1 1 f f 1 1 1 1 1 f f 
+f 1 1 1 1 1 1 f f 1 1 1 1 1 f f 
+f 1 1 1 1 1 1 f f 1 1 1 1 1 f f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 f f 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 f f f f f f f f f f f 
+f 1 1 1 1 1 f f f f f f f f f f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f f f f f f f f f f f f f f f f 
+`, img`
+f f f f f f f f f f f f f f f f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 c c 1 1 1 c c 1 f 
+f 1 1 1 1 1 1 c c 1 1 1 c c 1 f 
+f 1 1 1 1 1 1 c c 1 1 1 c c 1 f 
+f 1 1 1 1 1 1 c c 1 1 1 c c 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 f f 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 f f f f f f f f f f f f f f 
+f 1 1 f f f f f f f f f f f f f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f f f f f f f f f f f f f f f f 
+`, img`
+f f f f f f f f f f f f f f f f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 f f 1 1 1 1 1 c c 1 f 
+f 1 1 1 1 f f 1 1 1 1 1 c c 1 f 
+f 1 1 1 1 f f 1 1 1 1 1 c c 1 f 
+f 1 1 1 1 f f 1 1 1 1 1 c c 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 f f 1 1 1 1 1 1 1 1 1 1 f f 
+f 1 f f f f f f f f f f f f f f 
+f 1 1 f f f f f f f f f f f 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f f f f f f f f f f f f f f f f 
+`, img`
+f f f f f f f f f f f f f f f f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 f f 1 1 f f 1 1 1 1 f 
+f 1 1 1 1 f f 1 1 f f 1 1 1 1 f 
+f 1 1 1 1 f f 1 1 f f 1 1 1 1 f 
+f 1 1 1 1 f f 1 1 f f 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 f f 1 1 1 1 1 1 1 1 f f 1 f 
+f 1 f f f f f f f f f f f f 1 f 
+f 1 1 f f f f f f f f f f 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f f f f f f f f f f f f f f f f 
+`, img`
+f f f f f f f f f f f f f f f f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 c c 1 1 1 1 1 f f 1 1 1 1 f 
+f 1 c c 1 1 1 1 1 f f 1 1 1 1 f 
+f 1 c c 1 1 1 1 1 f f 1 1 1 1 f 
+f 1 c c 1 1 1 1 1 f f 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f f 1 1 1 1 1 1 1 1 1 1 f f 1 f 
+f f f f f f f f f f f f f f 1 f 
+f 1 f f f f f f f f f f f 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f f f f f f f f f f f f f f f f 
+`, img`
+f f f f f f f f f f f f f f f f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 c c 1 1 1 c c 1 1 1 1 1 1 f 
+f 1 c c 1 1 1 c c 1 1 1 1 1 1 f 
+f 1 c c 1 1 1 c c 1 1 1 1 1 1 f 
+f 1 c c 1 1 1 c c 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 f f 1 f 
+f f f f f f f f f f f f f f 1 f 
+f f f f f f f f f f f f f 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f f f f f f f f f f f f f f f f 
+`, img`
+f f f f f f f f f f f f f f f f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f f 1 1 1 1 1 f f 1 1 1 1 1 1 f 
+f f 1 1 1 1 1 f f 1 1 1 1 1 1 f 
+f f 1 1 1 1 1 f f 1 1 1 1 1 1 f 
+f f 1 1 1 1 1 f f 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 f f 1 1 1 1 f 
+f f f f f f f f f f f 1 1 1 1 f 
+f f f f f f f f f f 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f f f f f f f f f f f f f f f f 
+`, img`
+f f f f f f f f f f f f f f f f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 f f 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 f f 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 f f 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 f f 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 f f 1 1 1 1 1 1 1 f 
+f f f f f f f f 1 1 1 1 1 1 1 f 
+f f f f f f 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f f f f f f f f f f f f f f f f 
+`, img`
+f f f f f f f f f f f f f f f f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f f f f f f f f f f f f f f f f 
+`];
+            case "myAnim":
+            case "anim2":return [img`
+f f f f f f f f f f f f f f f f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 f f 1 1 1 1 1 1 1 1 f f 1 f 
+f 1 f f 1 1 1 1 1 1 1 1 f f 1 f 
+f 1 f f 1 1 1 1 1 1 1 1 f f 1 f 
+f 1 f f 1 1 1 1 1 1 1 1 f f 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 f 1 1 1 1 1 1 1 1 1 1 f 1 f 
+f 1 f 1 1 1 1 1 1 1 1 1 1 f 1 f 
+f 1 f 1 1 1 1 1 1 1 1 1 1 f 1 f 
+f 1 f 1 1 1 1 1 1 1 1 1 1 f 1 f 
+f 1 f f f f f f f f f f f f 1 f 
+f 1 f f f f f f f f f f f f 1 f 
+f f f f f f f f f f f f f f f f 
+`, img`
+f f f f f f f f f f f f f f f f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 f f 1 1 1 1 1 1 1 1 f f 1 f 
+f 1 f f 1 1 1 1 1 1 1 1 f f 1 f 
+f 1 f f 1 1 1 1 1 1 1 1 f f 1 f 
+f 1 f f 1 1 1 1 1 1 1 1 f f 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 f 1 1 1 1 1 1 1 1 1 1 f 1 f 
+f 1 f 1 1 1 1 1 1 1 1 1 1 f 1 f 
+f 1 f 1 1 1 1 1 1 1 1 1 1 f 1 f 
+f 1 f 1 1 1 1 1 1 1 1 1 1 f 1 f 
+f 1 f f f f f f f f f f f f 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f f f f f f f f f f f f f f f f 
+`, img`
+f f f f f f f f f f f f f f f f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 f f 1 1 1 1 1 1 1 1 f f 1 f 
+f 1 f f 1 1 1 1 1 1 1 1 f f 1 f 
+f 1 f f 1 1 1 1 1 1 1 1 f f 1 f 
+f 1 f f 1 1 1 1 1 1 1 1 f f 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 f 1 1 1 1 1 1 1 1 1 1 f 1 f 
+f 1 f 1 1 1 1 1 1 1 1 1 1 f 1 f 
+f 1 f 1 1 1 1 1 1 1 1 1 1 f 1 f 
+f 1 f 1 1 1 f f f f 1 1 1 f 1 f 
+f 1 f f f f f f f f f f f f 1 f 
+f 1 f f f f f f f f f f f f 1 f 
+f 1 f f f f f f f f f f f f 1 f 
+f 1 f f f 1 1 1 1 1 1 1 1 1 1 f 
+f f f f f f f f f f f f f f f f 
+`, img`
+f f f f f f f f f f f f f f f f 
+f 1 f f 1 1 1 1 1 1 1 1 f f 1 f 
+f 1 f f 1 1 1 1 1 1 1 1 f f 1 f 
+f 1 f f 1 1 1 1 1 1 1 1 f f 1 f 
+f 1 f f 1 1 1 1 1 1 1 1 f f 1 f 
+f 1 f f 1 1 1 1 1 1 1 1 f f 1 f 
+f 1 f 1 1 1 1 1 1 1 1 1 1 f 1 f 
+f 1 f 1 1 1 1 1 1 1 1 1 1 f 1 f 
+f 1 f 1 1 1 1 1 1 1 1 1 1 f 1 f 
+f 1 f 1 1 1 1 1 1 1 1 1 1 f 1 f 
+f 1 f 1 1 1 1 1 1 1 1 1 1 f 1 f 
+f 1 f 1 1 1 1 1 1 1 1 1 1 f 1 f 
+f 1 f f f f f f f f f f f f 1 f 
+f 1 f f f f f f f f f f f f 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f f f f f f f f f f f f f f f f 
+`, img`
+f f f f f f f f f f f f f f f f 
+f 1 f f 1 1 1 1 1 1 1 1 f f 1 f 
+f 1 f f 1 1 1 1 1 1 1 1 f f 1 f 
+f 1 f f 1 1 1 1 1 1 1 1 f f 1 f 
+f 1 f f 1 1 1 1 1 1 1 1 f f 1 f 
+f 1 f f 1 1 1 1 1 1 1 1 f f 1 f 
+f 1 f 1 1 1 1 1 1 1 1 1 1 f 1 f 
+f 1 f 1 1 1 1 1 1 1 1 1 1 f 1 f 
+f 1 f 1 1 1 1 1 1 1 1 1 1 f 1 f 
+f 1 f f f f f f f f f f f f 1 f 
+f 1 f f f f f f f f f f f f 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f f f f f f f f f f f f f f f f 
+`, img`
+f f f f f f f f f f f f f f f f 
+f 1 f f 1 1 1 1 1 1 1 1 f f 1 f 
+f 1 f f 1 1 1 1 1 1 1 1 f f 1 f 
+f 1 f f 1 1 1 1 1 1 1 1 f f 1 f 
+f 1 f f 1 1 1 1 1 1 1 1 f f 1 f 
+f 1 f f 1 1 1 1 1 1 1 1 f f 1 f 
+f 1 f 1 1 1 1 1 1 1 1 1 1 f 1 f 
+f 1 f 1 1 1 1 1 1 1 1 1 1 f 1 f 
+f 1 f f f f f f f f f f f f 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f f f f f f f f f f f f f f f f 
+`, img`
+f f f f f f f f f f f f f f f f 
+f 1 f f 1 1 1 1 1 1 1 1 f f 1 f 
+f 1 f f 1 1 1 1 1 1 1 1 f f 1 f 
+f 1 f f 1 1 1 1 1 1 1 1 f f 1 f 
+f 1 f f 1 1 1 1 1 1 1 1 f f 1 f 
+f 1 f f 1 1 1 1 1 1 1 1 f f 1 f 
+f 1 f 1 1 1 1 1 1 1 1 1 1 f 1 f 
+f 1 f f f f f f f f f f f f 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f f f f f f f f f f f f f f f f 
+`, img`
+f f f f f f f f f f f f f f f f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f f f f f f f f f f f f f f f f 
+`, img`
+f f f f f f f f f f f f f f f f 
+f 1 f f f f f f f f f f f f 1 f 
+f 1 f f f f f f f f f f f f 1 f 
+f 1 f 1 1 1 1 1 1 1 1 1 1 f 1 f 
+f 1 f 1 1 1 1 1 1 1 1 1 1 f 1 f 
+f 1 f 1 1 1 1 1 1 1 1 1 1 f 1 f 
+f 1 f 1 1 1 1 1 1 1 1 1 1 f 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 f f 1 1 1 1 1 1 1 1 f f 1 f 
+f 1 f f 1 1 1 1 1 1 1 1 f f 1 f 
+f 1 f f 1 1 1 1 1 1 1 1 f f 1 f 
+f 1 f f 1 1 1 1 1 1 1 1 f f 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f f f f f f f f f f f f f f f f 
+`, img`
+f f f f f f f f f f f f f f f f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 f f f f f f f f f f f f 1 f 
+f 1 f 1 1 1 1 1 1 1 1 1 1 f 1 f 
+f 1 f 1 1 1 1 1 1 1 1 1 1 f 1 f 
+f 1 f 1 1 1 1 1 1 1 1 1 1 f 1 f 
+f 1 f 1 1 1 1 1 1 1 1 1 1 f 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 f f 1 1 1 1 1 1 1 1 f f 1 f 
+f 1 f f 1 1 1 1 1 1 1 1 f f 1 f 
+f 1 f f 1 1 1 1 1 1 1 1 f f 1 f 
+f 1 f f 1 1 1 1 1 1 1 1 f f 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f f f f f f f f f f f f f f f f 
+`, img`
+f f f f f f f f f f f f f f f f 
+f 1 f f f 1 1 1 1 1 1 1 1 1 1 f 
+f 1 f f f f f f f f f f f f 1 f 
+f 1 f f f f f f f f f f f f 1 f 
+f 1 f f f f f f f f f f f f 1 f 
+f 1 f 1 1 1 f f f f 1 1 1 f 1 f 
+f 1 f 1 1 1 1 1 1 1 1 1 1 f 1 f 
+f 1 f 1 1 1 1 1 1 1 1 1 1 f 1 f 
+f 1 f 1 1 1 1 1 1 1 1 1 1 f 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 f f 1 1 1 1 1 1 1 1 f f 1 f 
+f 1 f f 1 1 1 1 1 1 1 1 f f 1 f 
+f 1 f f 1 1 1 1 1 1 1 1 f f 1 f 
+f 1 f f 1 1 1 1 1 1 1 1 f f 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f f f f f f f f f f f f f f f f 
+`, img`
+f f f f f f f f f f f f f f f f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 f f f f f f f f f f f f 1 f 
+f 1 f f f f f f f f f f f f 1 f 
+f 1 f 1 1 1 1 1 1 1 1 1 1 f 1 f 
+f 1 f 1 1 1 1 1 1 1 1 1 1 f 1 f 
+f 1 f 1 1 1 1 1 1 1 1 1 1 f 1 f 
+f 1 f 1 1 1 1 1 1 1 1 1 1 f 1 f 
+f 1 f 1 1 1 1 1 1 1 1 1 1 f 1 f 
+f 1 f 1 1 1 1 1 1 1 1 1 1 f 1 f 
+f 1 f f 1 1 1 1 1 1 1 1 f f 1 f 
+f 1 f f 1 1 1 1 1 1 1 1 f f 1 f 
+f 1 f f 1 1 1 1 1 1 1 1 f f 1 f 
+f 1 f f 1 1 1 1 1 1 1 1 f f 1 f 
+f 1 f f 1 1 1 1 1 1 1 1 f f 1 f 
+f f f f f f f f f f f f f f f f 
+`, img`
+f f f f f f f f f f f f f f f f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 f f f f f f f f f f f f 1 f 
+f 1 f 1 1 1 1 1 1 1 1 1 1 f 1 f 
+f 1 f 1 1 1 1 1 1 1 1 1 1 f 1 f 
+f 1 f f 1 1 1 1 1 1 1 1 f f 1 f 
+f 1 f f 1 1 1 1 1 1 1 1 f f 1 f 
+f 1 f f 1 1 1 1 1 1 1 1 f f 1 f 
+f 1 f f 1 1 1 1 1 1 1 1 f f 1 f 
+f 1 f f 1 1 1 1 1 1 1 1 f f 1 f 
+f f f f f f f f f f f f f f f f 
+`, img`
+f f f f f f f f f f f f f f f f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 f f f f f f f f f f f f 1 f 
+f 1 f 1 1 1 1 1 1 1 1 1 1 f 1 f 
+f 1 f f 1 1 1 1 1 1 1 1 f f 1 f 
+f 1 f f 1 1 1 1 1 1 1 1 f f 1 f 
+f 1 f f 1 1 1 1 1 1 1 1 f f 1 f 
+f 1 f f 1 1 1 1 1 1 1 1 f f 1 f 
+f 1 f f 1 1 1 1 1 1 1 1 f f 1 f 
+f f f f f f f f f f f f f f f f 
+`, img`
+f f f f f f f f f f f f f f f f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
+f f f f f f f f f f f f f f f f 
+`];
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
